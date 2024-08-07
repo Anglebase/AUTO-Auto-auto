06c377be9e5f8fcdc3e87fe8804d4d2c1bd384ec
Anglebase <1628818324@qq.com>
2024-08-07 17:56:29 +0800

    优化链接工具

--- linker.py
+++ linker.py
@@ -1,28 +1,30 @@
 import log, os
-from comlier import issource
+from comlier import issource, isWindows, isLinux
 
 
 def help():
     """
     命令格式: -l [库类别] {库名} {链接源} [链接源 [...]] [选项 [...]]
         库类别可以是以下值之一：
-            /help      显示此帮助信息
-            /dll       指定输出目标为 Windows 风格动态链接库(<库名>.dll)
-            /lib       指定输出目标为 Windows 风格静态链接库(<库名>.lib)
-            /so        指定输出目标为 Linux 风格动态链接库(lib<库名>.so)
-            /a         指定输出目标为 Linux 风格静态链接库(lib<库名>.a)
+            /help           显示此帮助信息
+            /shared         指定输出目标为动态链接库
+            /static         指定输出目标为静态链接库
         链接源可以是以下值之一：
-            /at=<项目路径>          指定链接文件的源项目路径(默认为当前执行目录[.\])
-            /file+=<文件路径>       指定链接源为指定文件，若为相对路径，则相对于当前执行目录[.\]
-            /path+=<目录路径>       指定链接源为指定目录下的所有文件，若为相对路径，则相对于当前执行目录[.\]
+            /at=            指定链接文件的源项目路径(默认为当前执行目录[.\])
+            /file+=         指定链接源为指定文件，若为相对路径，则相对于当前执行目录[.\]
+            /path+=         指定链接源为指定目录下的所有文件，若为相对路径，则相对于当前执行目录[.\]
         选项可以是以下值之一：
+            /unix           强制使用Linux命名风格(libxxx.a, libxxx.so)
+            /win            强制使用Windows命名风格(xxx.lib, xxx.dll)
+            /lnkr=          指定静态链接器路径(仅当库类别为/lib或/a时有效)，默认为'ar'
+            /L=             指定链接库搜索路径(仅当库类别为/dll或/so时有效)
+            /l=             指定链接库(仅当库类别为/dll或/so时有效)
             若要为该选项指定多个值，请用逗号分隔，例如：-L=path1,path2,path3、-l=lib1,lib2
-            /L=         指定链接库搜索路径(仅当库类别为/dll或/so时有效)
-            /l=         指定链接库(仅当库类别为/dll或/so时有效)
     """
     print(help.__doc__)
 
 
+linker_dir = "ar"
 link_file = []
 link_path = []
 output_type = ""
@@ -36,21 +38,35 @@ l_libs = []
 def set_optioins(options: list):
     if len(options) < 3:
         log.ERROR("缺少必要参数")
-        return
+        return False
 
     global output_type
-    if options[0] in ["/dll", "/lib", "/a", "/so"]:
-        output_type = options[0][1:]
+    if options[0] in ["/shared", "/static"]:
+        if isWindows():
+            if options[0] == "/shared":
+                output_type = "dll"
+            else:
+                output_type = "lib"
+        elif isLinux():
+            if options[0] == "/shared":
+                output_type = "so"
+            else:
+                output_type = "a"
+        else:
+            log.ERROR("不支持当前系统")
+            return False
     else:
         log.ERROR(f"无效的输出目标'{options[0]}'")
-        return
+        return False
 
     global output_name
     output_name = options[1]
     for ch in output_name:
         if not ch.isalnum() and ch not in ["_", "."]:
             log.ERROR(f"输出文件名'{output_name}'不合法")
-            return
+            return False
+
+    nametype = "windows" if isWindows() else "linux"
 
     for i in range(2, len(options)):
         if options[i].startswith("/file+="):
@@ -77,16 +93,38 @@ def set_optioins(options: list):
             L_dirs.extend(options[i][3:].split(","))
         elif options[i].startswith("/l="):
             l_libs.extend(options[i][3:].split(","))
+        elif options[i].startswith("/lnkr="):
+            global linker_dir
+            linker_dir = options[i][6:]
+        elif options[i] in ["/unix", "/win"]:
+            if options[i] == "/unix":
+                nametype = "linux"
+            else:
+                nametype = "windows"
         else:
             log.WARNING(f"被忽略的无效参数'{options[i]}'")
-            return
+            return False
+
+        if nametype == "windows":
+            if output_type == "so":
+                output_type = "dll"
+            elif output_type == "a":
+                output_type = "lib"
+        elif nametype == "linux":
+            if output_type == "dll":
+                output_type = "so"
+            elif output_type == "lib":
+                output_type = "a"
+
+    return True
 
 
 def linker(options: list):
     if not options or options[0] == "/help":
         help()
         return
-    set_optioins(options)
+    if not set_optioins(options):
+        return
 
     for i in range(len(link_path)):
         link_path[i] = os.path.relpath(link_path[i], root_path)
@@ -119,24 +157,27 @@ def linker(options: list):
             log.DEBUG(f"包含目录: {dirs}")
             for file in files:
                 log.DEBUG(f"搜索文件: {file}")
-                if output_type in ["dll", "lib"]:
-                    if file.endswith(".obj"):
-                        files_list.append(os.path.join(root, file))
-                    elif issource(file):
-                        files_list.append(
-                            os.path.join(root, ".".join(file.split(".")[:-1]) + ".obj")
-                        )
+                if file.endswith(".obj") or file.endswith(".o"):
+                    files_list.append(os.path.join(root, file))
+                elif issource(file):
+                    win_obj_path = os.path.join(
+                        os.path.join(".build", root),
+                        ".".join(file.split(".")[:-1]) + ".obj",
+                    )
+                    unix_obj_path = os.path.join(
+                        os.path.join(".build", root),
+                        ".".join(file.split(".")[:-1]) + ".o",
+                    )
+                    log.DEBUG(win_obj_path)
+                    log.DEBUG(unix_obj_path)
+                    if os.path.exists(win_obj_path):
+                        files_list.append(win_obj_path)
+                    elif os.path.exists(unix_obj_path):
+                        files_list.append(unix_obj_path)
                     else:
-                        log.WARNING(f"忽略非源文件非目标文件'{file}'")
-                elif output_type in ["a", "so"]:
-                    if file.endswith(".o"):
-                        files_list.append(os.path.join(root, file))
-                    elif issource(file):
-                        files_list.append(
-                            os.path.join(root, ".".join(file.split(".")[:-1]) + ".o")
-                        )
-                    else:
-                        log.WARNING(f"忽略非源文件非目标文件'{file}'")
+                        log.WARNING(f"未找到源文件'{file}'的目标文件")
+                else:
+                    log.WARNING(f"忽略非源文件非目标文件'{file}'")
 
     log.DEBUG(f"链接源文件: {files_list}")
     if not files_list:
@@ -165,14 +206,16 @@ def linker(options: list):
     log.DEBUG(f"链接库搜索路径: {L_args}")
     log.DEBUG(f"链接库: {l_args}")
 
+    global linker_dir
+    log.DEBUG(f"静态链接器路径: {linker_dir}")
     if output_type == "lib":
-        linker_cmd = f"ar rcs {output_path}.lib {' '.join(files_list)}"
+        linker_cmd = f"{linker_dir} rs {output_path}.lib {' '.join(files_list)}"
     elif output_type == "dll":
         linker_cmd = (
             f"g++ -shared -o {output_path}.dll {' '.join(files_list)} {L_args} {l_args}"
         )
     elif output_type == "a":
-        linker_cmd = f"ar rcs {output_path_unix}.a {' '.join(files_list)}"
+        linker_cmd = f"{linker_dir} rs {output_path_unix}.a {' '.join(files_list)}"
     elif output_type == "so":
         linker_cmd = f"g++ -shared -o {output_path_unix}.so {' '.join(files_list)} {L_args} {l_args}"
 
